f14d01096ff91942ccff86778d3c98db77fd8c44
HabarovIM <habarovim@mer.ci.nsu.ru>
2022-03-30 12:10:21 +0700

    Добавьте файлы проекта.

---
+++ Dll3_App/DllLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public static class DllLib
+    {
+        [DllImport(@"C:\Users\Юлия Хабарова\Desktop\Dll Hab\DLL3\x64\Debug\Dll3.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr LoadPicture([MarshalAs(UnmanagedType.BStr)] string path, int newColor, int borderSize);
+    }
+}
---
+++ Dll3_App/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp1;
+
+namespace Dll3_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                pictureBox1.Width = image.Width;
+                pictureBox1.Height = image.Height;
+                IntPtr ptr = DllLib.LoadPicture(dialog.FileName, 500, 20);
+                Image image1 = Image.FromHbitmap(ptr);
+                pictureBox1.Image = image1;
+            }
+
+        }
+    }
+}
